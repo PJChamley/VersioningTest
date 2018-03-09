c4a6ec61fd60ca9c0c447086b8f863eef9f5a88f
Peter Chamley <peter@chamley.name>
2018-03-09 13:13:14 +0000

    Add Interface Project.

---
+++ src/VersionTesting.Interface/IMyClassToGetData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionTesting.Interface
+{
+    public interface IMyClassToGetData
+    {
+        string GetSomeData { get; set; }
+        //string GetMoreData { get; set; }
+        //string AddedAsPartOf5 { get; set; }
+    }
+}
